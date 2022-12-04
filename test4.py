77e165ab4900c311ab869a3c0d864c469658a5dc
jeremyas2002 <116431942+jeremyas2002@users.noreply.github.com>
2022-12-04 23:17:37 +0700

    Delete test4.py

--- test4.py
+++
@@ -1,9 +0,0 @@
-a = int(input("masukan angka inputan1:  "))
-b = int(input("masukan angka inputan2:  "))
-
-if a != b:
-    print("Angka berbeda")
-if a==b:
-    print("Angka kembar!")
-else:
-    pass
